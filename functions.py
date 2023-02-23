5a56bb8b6b09afe4c9a8a4e6115c7d12c9b6bbb4
Xerozzz <60227509+Xerozzz@users.noreply.github.com>
2023-02-23 22:35:06 +0800

    Separate function file for validation

---
+++ functions.py
@@ -0,0 +1,17 @@
+from flask import Flask, url_for, render_template, flash, redirect, request, jsonify, make_response, send_file
+
+def input_validation_check(name, order, session):
+    ret = False
+    values = {
+        "Name": name,
+        "Order": order, 
+        "Session": session
+    }
+    for key,value in values.items():
+        if not value:
+            flash(f'{key} is required!', 'danger')
+            ret = True
+    if len(session) != 5 :
+        flash('Session ID is must be 5 digits!', 'danger')
+        ret = True
+    return ret
