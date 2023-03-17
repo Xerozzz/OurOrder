5dc82d7d2c370aa804c161bdabd09c2cf7aafea2
Xerozzz <60227509+Xerozzz@users.noreply.github.com>
2023-03-17 22:52:57 +0800

    Added error handling for functions

--- functions.py
+++ functions.py
@@ -6,17 +6,20 @@ def input_validation_check(name, order, session):
     Validating orders input, to ensure name, 
     order and session are input and within correct parameters
     """
-    ret = False
-    values = {
-        "Name": name,
-        "Order": order,
-        "Session": session
-    }
-    for key, value in values.items():
-        if not value:
-            flash(f'{key} is required!', 'danger')
+    try:
+        ret = False
+        values = {
+            "Name": name,
+            "Order": order,
+            "Session": session
+        }
+        for key, value in values.items():
+            if not value:
+                flash(f'{key} is required!', 'danger')
+                ret = True
+        if len(session) != 5 or session.isdigit() == False:
+            flash('Session ID is must be 5 digits!', 'danger')
             ret = True
-    if len(session) != 5:
-        flash('Session ID is must be 5 digits!', 'danger')
-        ret = True
-    return ret
+        return ret
+    except Exception as error:
+        print("Yeet",error)
